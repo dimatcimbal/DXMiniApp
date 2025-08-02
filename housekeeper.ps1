854a24fef109da0466e382355f94a606c6dfc35c
Dmitrii Tcimbal <dtcimbal@gmail.com>
2025-08-03 09:46:59 +1000

    set -Debug flag for the housekeeper script for Build Project (Debug) action

--- housekeeper.ps1
+++ housekeeper.ps1
@@ -1,4 +1,4 @@
 ﻿# Wrapper for the housekeeper.ps1 script from git@github.com:dimatcimbal/Housekeeper.git
-# $scriptPath = Join-Path (Get-Item -Path $PSScriptRoot).FullName "..\Housekeeper\housekeeper.ps1"
+#$scriptPath = Join-Path (Get-Item -Path $PSScriptRoot).FullName "..\Housekeeper\housekeeper.ps1"
 $scriptPath = Join-Path (Get-Item -Path $PSScriptRoot).FullName "Housekeeper\housekeeper.ps1"
 & $scriptPath @Args
